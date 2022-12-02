2a480aefaa353c43b5f77e92c4fc8e42d6b81585
christos-golsouzidis <chr.golsou@gmail.com>
2022-12-02 14:30:53 +0100

    first commit

---
+++ templates/strapp.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec  2 10:44:27 2022
+
+@author: Chris
+"""
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import plotly as 
+import seaborn as sns
+import streamlit as st
+
+st.title('asdfghjk')
+st.write("**Starting** the *build* of penguin")
+st.write('Data is taken from [palmerpenguins](https://google.com)')
